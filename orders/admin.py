6c26da0bf2d65f374be938c77c35fd091cb385a8
Yeghishe <ligan77770@gmail.com>
2023-07-04 13:45:54 +0400

    Please enter the commit message for your changes. Lines starting   with '#' will be ignored, and an empty message aborts the commit.   On branch main   Your branch is up to date with 'origin/main'.   Changes to be committed:   	modified:   db.sqlite3   	new file:   media/products/22.jpg   	new file:   media/products/38.jpg   	new file:   media/products/38_8PqR2PH.jpg   	new file:   media/products/38_8WrLga3.jpg   	new file:   media/products/38_GY4VUYn.jpg   	new file:   media/products/38_LPm6PHE.jpg   	new file:   media/products/38_qUBATHh.jpg   	new file:   media/products/8.jpg   	new file:   media/products/RE4OXzi.png   	new file:   media/products/c3.jpg   	new file:   media/products/im6.jpg   	new file:   media/products/images.jpg   	new file:   media/products/images_2.jpg   	new file:   media/products/images_3.jpg   	new file:   media/products/iph.jpg   	new file:   media/products/ph1.jpg   	new file:   media/products/ph2.jpg   	new file:   media/products/ph3.png   	new file:   "media/products/\320\221\320\265\320\267_\320\275\320\260\320\267\320\262\320\260\320\275\320\270\321\217.jpg"   	modified:   orders/__pycache__/admin.cpython-311.pyc   	modified:   orders/admin.py   	modified:   shop/__pycache__/views.cpython-311.pyc

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created', 'status', 'get_total_price')
+    list_filter = ('status',)
+    search_fields = ('user__username', 'user__email', 'user__full_name')
+    inlines = [OrderItemInline]
+
+admin.site.register(Order, OrderAdmin)
